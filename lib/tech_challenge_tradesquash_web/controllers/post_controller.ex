430bc66aadea2ccedc27202b04406ed22fc039e2
Dickson <dicksonmelo@gmail.com>
2022-08-15 19:44:50 -0300

    feat: add logout

--- lib/tech_challenge_tradesquash_web/controllers/post_controller.ex
+++ lib/tech_challenge_tradesquash_web/controllers/post_controller.ex
@@ -19,7 +19,6 @@ defmodule TechChallengeTradesquashWeb.PostController do
 
   def create(conn, %{"post" => post_params}) do
     current_account = Authentication.get_current_account(conn)
-    IO.inspect(post_params)
 
     case Posts.create_post(post_params, current_account.id) do
       {:ok, post} ->
