c5c60fbb1869573df8167c006f2c1c28e591a812
Dickson <dicksonmelo@gmail.com>
2022-08-16 16:40:15 -0300

    assets config

--- lib/tech_challenge_tradesquash_web/endpoint.ex
+++ lib/tech_challenge_tradesquash_web/endpoint.ex
@@ -22,9 +22,9 @@ defmodule TechChallengeTradesquashWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/",
-    from: :tech_challenge_tradesquash,
+    from: "/priv/assets",
     gzip: true,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts css images favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
